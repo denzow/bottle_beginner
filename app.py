3bab795e4cfdb100d70a4a81f8d316682653b558
sayamada <sayamada@ashisuto.co.jp>
2016-09-17 13:29:48 +0900

    ダイナミックルーティング
    * realoderの有効化
    * 複数のroute
    * URLからパラメータ取得
    * 型指定でのパラメータ取得

--- app.py
+++ app.py
@@ -2,9 +2,49 @@
 from bottle import route, run
 
 
+@route('/')
 @route('/hello')
 def hello():
+    """
+    route でどのようなURLでアクセスした際に関数が実行されるかを
+    登録します。
+
+    文字列をreturnすると、そのままブラウザで表示されます
+
+    routeでのルーティングは重ねる事で、複数のURLにマップさせる事もできます。
+    :return:
+    """
     return "Hello World!"
 
-run(host='localhost', port=8080, debug=True)
+
+@route("/hello/<name>")
+def hello_name(name):
+    """
+    ダイナミックルーティングではurlに埋め込まれたパラメータを
+    取得することができます
+
+    :param name:
+    :return:
+    """
+    return "Hello {} !".format(name)
+
+
+@route("/no/<your_no:int>")
+def no(your_no):
+    """
+    ルーティングでは数字のみ・正規表現等で細かく設定できます
+
+    eg.
+    /no/100 -> OK
+    /no/one -> NG
+
+    :param your_no:
+    :return:
+    """
+
+    return "Your No is {} !".format(your_no)
+
+
+# サーバの起動
+run(host='localhost', port=8080, debug=True, reloader=True)
 
