efa6b20fecbbb952fdfbee612ec00958f667bf3e
sayamada <sayamada@ashisuto.co.jp>
2016-09-17 14:19:05 +0900

    入室時の名前処理
    * フォームデータをcookieに格納

--- chat.py
+++ chat.py
@@ -1,12 +1,31 @@
 # coding:utf-8
 import bottle
-from bottle import route, run, template
+from bottle import route, run, template, request, response
 
 
 @route("/")
 def index():
-
     return template("index")
+
+
+@route("/enter", method=["POST"])
+def enter():
+    """
+    入室処理を行います。
+    フォームの情報をcookieに格納し、チャット時の名前として使用します。
+
+    :return:
+    """
+    # POSTデータの確認
+    username = request.POST.get("username")
+    print("POST username is ", username)
+
+    # cookieへの格納
+    response.set_cookie("username", username)
+
+    return ""
+
+
 
 # サーバの起動
 run(host='localhost', port=8080, debug=True, reloader=True)
